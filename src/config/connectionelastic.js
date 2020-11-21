22fcefd7f14493d504017da5a4f3e580f91456c5
Juliano de Mello <julianodemello1999@gmail.com>
2020-11-21 10:17:36 -0500

    ajustando conexao elastic

--- src/config/connectionelastic.js
+++ src/config/connectionelastic.js
@@ -2,11 +2,14 @@ var elasticsearch=require('elasticsearch');
 require('dotenv').config();
 
 
+
+
+
 var client = new elasticsearch.Client( {  
   
   hosts: [
-    `http://${process.env.USER_ELASTIC}:${process.env.PASSWORD_ELASTIC}@${process.env.HOST_ELASTIC}:${process.env.PORT_ELASTIC}/`
-    //`http://${process.env.HOST_ELASTIC}:${process.env.PORT_ELASTIC}/`,
+    process.env.USER_ELASTIC==''?`http://${process.env.HOST_ELASTIC}:${process.env.PORT_ELASTIC}/`:
+`http://${process.env.USER_ELASTIC}:${process.env.PASSWORD_ELASTIC}@${process.env.HOST_ELASTIC}:${process.env.PORT_ELASTIC}/`
   ]
 });
 
